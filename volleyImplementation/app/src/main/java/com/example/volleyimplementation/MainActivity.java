15c32c7d8ad6c1b02eeae16e547070cf55ae6a19
ubaidghani <65225587+ubaidghani@users.noreply.github.com>
2020-05-17 21:51:19 +0500

    second commit

--- volleyImplementation/app/src/main/java/com/example/volleyimplementation/MainActivity.java
+++ volleyImplementation/app/src/main/java/com/example/volleyimplementation/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.textclassifier.TextLinks;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListAdapter;
 import android.widget.ListView;
@@ -16,6 +17,7 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 import com.squareup.picasso.Picasso;
@@ -31,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
     private Button profiles;
     private RequestQueue requestQueue;
     private ImageView imageView;
+    private EditText uid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +41,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         results = (TextView) findViewById(R.id.result);
         profiles = (Button) findViewById(R.id.profile);
+        uid = (EditText) findViewById(R.id.uid);
 
 
 
@@ -46,22 +50,25 @@ public class MainActivity extends AppCompatActivity {
         profiles.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                getProfiles();
+                int userid = Integer.parseInt(uid.getText().toString());
+                getProfiles(userid);
                 profiles.setVisibility(View.GONE);
+                uid.setVisibility(View.GONE);
             }
         });
     }
 
-    private void getProfiles(){
+    private void getProfiles(final int usern){
         String url = "https://reqres.in/api/users?page=1";
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+        final JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
             @Override
             public void onResponse(JSONObject response) {
                 try {
                     JSONArray jsonArray = response.getJSONArray("data");
 
-                    for(int i = 0 ; i <jsonArray.length(); i++){
-                        JSONObject user = jsonArray.getJSONObject(i);
+                   //for(int i = 0 ; i <jsonArray.length(); i++){
+                        JSONObject user = jsonArray.getJSONObject(usern-1);
 
                         int id = user.getInt("id");
                         String email = user.getString("email");
@@ -69,9 +76,11 @@ public class MainActivity extends AppCompatActivity {
                         String last_name = user.getString("last_name");
                         String avatar = user.getString("avatar");
 
-                        results.append("ID: "+String.valueOf(id) + "\n" + "Email: "+email + "\n" +"First Name: "+ first_name + "\n" + "Last Name: "+last_name + "\n" + "Profile: "+ avatar +"\n\n");
+                        results.append("ID: "+String.valueOf(id) + "\n" + "Email: "+email + "\n" +"First Name: "+ first_name + "\n" + "Last Name: "+last_name + "\n\n");
+                        imageView=(ImageView)findViewById(R.id.imageView);
+                        Picasso.get().load(avatar).into(imageView);
 
-                   }
+                  //}
 
 
                 } catch (JSONException e) {
@@ -84,6 +93,7 @@ public class MainActivity extends AppCompatActivity {
                 error.printStackTrace();
             }
         });
-        requestQueue.add(jsonObjectRequest);
+       requestQueue.add(jsonObjectRequest);
+
     }
 }
